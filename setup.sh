83cc8eba63cc27ad171295ca458012795c33782d
gpt-abacaba-dev <abacabaexplorer@gmail.com>
2025-07-24 11:58:44 +0200

    Nettoyage : suppression doublons, renommage doc → docs, MAJ .gitignore

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/ocean-sentinelle/ocean-sentinelle.git
-cd ocean-sentinelle
-cp -r ../ocean-sentinelle-config/* .
-git add .
-git commit -m "Ajout de la structure de configuration minimale Copilote OCEAN-SENTINELLE"
